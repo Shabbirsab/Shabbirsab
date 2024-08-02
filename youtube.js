e7db75cc35ce2a7c89869e5c4ec05b07f8071b42
Shabbirsab <sameermanale7070@gmail.com>
2024-08-02 13:53:22 +0530

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburgerMenu.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+});
